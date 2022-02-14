a26d36df3d0ef33b83a1c66f7393bba60553e141
Scott T Lyman <s.t.lyman84@gmail.com>
2022-02-14 15:41:12 -0700

    all classes created

---
+++ app/Models/Animals.js
@@ -0,0 +1,444 @@
+import { Alert } from "bootstrap";
+
+export class Alligator {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Bobcat {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Cheetah {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Dolphin {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Elephant {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Falcon {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Giraffe {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Hawk {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Iguana {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Jaguar {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Koala {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Lemur {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Marmoset {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Narwhal {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Octopus {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Panther {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Quail {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Raccoon {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Sable {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Tiger {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Urchin {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Vicuna {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches,
+      this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Wallaby {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Xeme {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Yak {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+export class Zebra {
+  constructor(name, gender, weight, tailLengthInches, canFly, hasClaws, hasScales) {
+    this.name = name;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInches = tailLengthInches;
+    this.canFly = canFly;
+    this.hasClaws = hasClaws;
+    this.hasScales = hasScales;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
